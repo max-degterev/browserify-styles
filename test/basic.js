9466dd154e0b3e826cc210ed67c0e976e1169180
Max Degterev <max@degterev.me>
2015-10-23 21:26:42 +0200

    added basic test

---
+++ test/basic.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var fs = require('fs');
+var expect = require('chai').expect;
+
+var validContents = fs.readFileSync(__dirname + '/fixtures/compiled.css', 'utf8');
+
+describe('browserify-styles', function() {
+  it('should bundle css files', function(done) {
+
+    var browserify = require('browserify'),
+        bundle = browserify({ entries: __dirname + '/../example/package/script.js' }),
+        writeStream = new (require('stream').Writable);
+
+    writeStream._write = function(){};
+
+    bundle.plugin(require('../'), {
+      output: __dirname + '../example/build/style.css',
+      modules: ['postcss', 'stylus', 'scss']
+    });
+
+    bundle.bundle().on('end', function(){
+      var compiled = fs.readFileSync(__dirname + '/../example/build/style.css', 'utf8');
+      expect(validContents).to.be.equal(compiled);
+
+      done();
+    }).pipe(writeStream);
+  });
+});
