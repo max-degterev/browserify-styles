e33d9ad210c6eb53c4b7a5afcd484e45e64c819f
Max Degterev <max@degterev.me>
2015-10-26 14:16:11 +0100

    fixed tests

--- test/basic.js
+++ test/basic.js
@@ -15,14 +15,32 @@ describe('browserify-styles', function() {
     writeStream._write = function(){};
 
     bundle.plugin(require('../'), {
-      output: __dirname + '../example/build/style.css',
+      output: __dirname + '/../example/build/style.css',
       modules: ['postcss', 'stylus', 'scss']
     });
 
-    bundle.bundle().on('end', function(){
+    bundle.bundle().on('css_end', function(){
       var compiled = fs.readFileSync(__dirname + '/../example/build/style.css', 'utf8');
       expect(validContents).to.be.equal(compiled);
 
+      done();
+    }).pipe(writeStream);
+  });
+
+  it('should emit css stream', function(done) {
+
+    var browserify = require('browserify'),
+        bundle = browserify({ entries: __dirname + '/../example/package/script.js' }),
+        writeStream = new (require('stream').Writable);
+
+    writeStream._write = function(){};
+
+    bundle.plugin(require('../'), {
+      modules: ['postcss', 'stylus', 'scss']
+    });
+
+    bundle.bundle().on('css_stream', function(stream){
+      expect(stream).to.exist;
       done();
     }).pipe(writeStream);
   });
